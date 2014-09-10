f95d7681339d3fec6c78f4065137da98caa778a9
Andrew Babichev <ababichev@dataart.com>
2014-09-10 11:18:40 +0300

    Create collections

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140908203933) do
+ActiveRecord::Schema.define(version: 20140910072934) do
+
+  create_table "collections", force: true do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "login"
