2f2d550524fd9b08e8abbf74645443666027f379
Andrew Babichev <ababichev@dataart.com>
2014-09-18 10:58:30 +0300

    Show collections or users on home page

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,30 +1,22 @@
 class CollectionsController < ApplicationController
   before_action :set_collection, only: [:show, :edit, :update, :destroy]
 
-  # GET /collections
-  # GET /collections.json
   def index
-    @collections = Collection.all
+    @collections = current_user.collections
   end
 
-  # GET /collections/1
-  # GET /collections/1.json
   def show
   end
 
-  # GET /collections/new
   def new
-    @collection = Collection.new
+    @collection = current_user.collections.build
   end
 
-  # GET /collections/1/edit
   def edit
   end
 
-  # POST /collections
-  # POST /collections.json
   def create
-    @collection = Collection.new(collection_params)
+    @collection = current_user.collections.build(collection_params)
 
     respond_to do |format|
       if @collection.save
@@ -37,37 +29,24 @@ class CollectionsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /collections/1
-  # PATCH/PUT /collections/1.json
   def update
-    respond_to do |format|
-      if @collection.update(collection_params)
-        format.html { redirect_to @collection, notice: 'Collection was successfully updated.' }
-        format.json { render :show, status: :ok, location: @collection }
-      else
-        format.html { render :edit }
-        format.json { render json: @collection.errors, status: :unprocessable_entity }
-      end
+    if @collection.update(collection_params)
+      redirect_to @collection, notice: 'Collection was successfully updated.'
+    else
+      render :edit
     end
   end
 
-  # DELETE /collections/1
-  # DELETE /collections/1.json
   def destroy
     @collection.destroy
-    respond_to do |format|
-      format.html { redirect_to collections_url, notice: 'Collection was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to collections_url, notice: 'Collection was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_collection
-      @collection = Collection.find(params[:id])
+      @collection = current_user.collections.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def collection_params
       params.require(:collection).permit(:name)
     end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    if current_user
+      @collections = current_user.collections
+    else
+      @users = User.all
+    end
   end
 end
